cb5e4eb4d147b8d1c9d12812e2b7ad149a35f11b
wenbaolin <blinwen@gmail.com>
2014-06-29 23:45:16 +0800

    modify

---
+++ src/com/douziapp/exam/data/SingleChoice.java
@@ -0,0 +1,73 @@
+package com.douziapp.exam.data;
+
+public class SingleChoice {
+
+	private String	selItemA;
+	private String	selItemB;
+	private String	selItemC;
+	private String	selItemD;
+	
+	private long	vID;
+	
+	private String	vContent;
+	
+	private long	rightItem;
+
+	public String getSelItemA() {
+		return selItemA;
+	}
+
+	public void setSelItemA(String selItemA) {
+		this.selItemA = selItemA;
+	}
+
+	public String getSelItemB() {
+		return selItemB;
+	}
+
+	public void setSelItemB(String selItemB) {
+		this.selItemB = selItemB;
+	}
+
+	public String getSelItemC() {
+		return selItemC;
+	}
+
+	public void setSelItemC(String selItemC) {
+		this.selItemC = selItemC;
+	}
+
+	public String getSelItemD() {
+		return selItemD;
+	}
+
+	public void setSelItemD(String selItemD) {
+		this.selItemD = selItemD;
+	}
+
+	public long getvID() {
+		return vID;
+	}
+
+	public void setvID(long vID) {
+		this.vID = vID;
+	}
+
+	public String getvContent() {
+		return vContent;
+	}
+
+	public void setvContent(String vContent) {
+		this.vContent = vContent;
+	}
+
+	public long getRightItem() {
+		return rightItem;
+	}
+
+	public void setRightItem(long rightItem) {
+		this.rightItem = rightItem;
+	}
+	
+	
+}
--- src/com/douziapp/exam/sipm/MainActivity.java
+++ src/com/douziapp/exam/sipm/MainActivity.java
@@ -1,15 +1,27 @@
 package com.douziapp.exam.sipm;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.douziapp.exam.slidingmenu.LeftFragment;
 import com.douziapp.exam.slidingmenu.RightFragment;
 import com.douziapp.exam.slidingmenu.SlidingMenu;
 import com.douziapp.exam.slidingmenu.ViewPageFragment;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class MainActivity extends FragmentActivity {
@@ -20,6 +32,9 @@ public class MainActivity extends FragmentActivity {
 	ViewPageFragment 	centerFragment;
 	
 	TextView			mImgExamBankManager;
+	
+	ListView			mExamIndex;
+	ExamIndexAdapter	mExamIndexAdapter;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +59,27 @@ public class MainActivity extends FragmentActivity {
 		mImgExamBankManager.setTypeface(font);
 		
 		mImgExamBankManager.setText(getString(R.string.ico_exam_bank_manager));
+		
+		mExamIndex = (ListView)findViewById(R.id.main_exam_index_view);
+
+		mExamIndexAdapter = new ExamIndexAdapter(this);
+		mExamIndex.setAdapter(mExamIndexAdapter);
+		
+		mExamIndex.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+					long arg3) {
+				
+				Intent intent = new Intent(MainActivity.this,ExamActivity.class);
+				
+				
+				startActivity(intent);
+				
+				//overridePendingTransition(android.R.anim.slide_in_left,
+				//		android.R.anim.slide_out_right);
+			}
+		});
 	}
 
 	private void init() {
@@ -76,5 +112,91 @@ public class MainActivity extends FragmentActivity {
 		t.commit();
 
 
+	}
+	
+	private static class ViewHolderItem{
+		TextView 	name_view;
+	}
+	
+	private class ExamIndexAdapter extends BaseAdapter{
+
+		private Context	mContext;
+		
+		private List<String>	mData;
+		
+		
+		
+		public ExamIndexAdapter(Context context){
+			this.mContext = context;
+			
+			mData = new ArrayList<String>();
+			
+			mData.add("2009年上半年上午试题分析与解答");
+			mData.add("2009年上半年下午试题分析与解答");
+			mData.add("2009年下半年上午试题分析与解答");
+			mData.add("2009年下半年下午试题分析与解答");
+			
+			mData.add("2010年上半年上午试题分析与解答");
+			mData.add("2010年上半年下午试题分析与解答");
+			mData.add("2010年下半年上午试题分析与解答");
+			mData.add("2010年下半年下午试题分析与解答");
+			
+			mData.add("2011年上半年上午试题分析与解答");
+			mData.add("2011年上半年下午试题分析与解答");
+			mData.add("2011年下半年上午试题分析与解答");
+			mData.add("2011年下半年下午试题分析与解答");
+			
+			mData.add("2012年上半年上午试题分析与解答");
+			mData.add("2012年上半年下午试题分析与解答");
+			mData.add("2012年下半年上午试题分析与解答");
+			mData.add("2012年下半年下午试题分析与解答");
+			
+		}
+		
+		@Override
+		public int getCount() {
+			
+			return mData.size();
+		}
+
+		@Override
+		public Object getItem(int position) {
+			
+			return mData.get(position);
+		}
+
+		@Override
+		public long getItemId(int position) {
+			
+			return position;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			
+			ViewHolderItem viewHolder;
+			
+			if(null == convertView ){
+				LayoutInflater inflater =(LayoutInflater) mContext.getSystemService(
+						Context.LAYOUT_INFLATER_SERVICE);
+				
+				convertView = inflater.inflate(R.layout.main_exam_index_item, null);
+				
+				viewHolder = new ViewHolderItem();
+
+				viewHolder.name_view = (TextView)convertView.findViewById(R.id.exam_name);
+
+				convertView.setTag(viewHolder);
+				
+			}else{
+				viewHolder = (ViewHolderItem)convertView.getTag();
+			}
+
+			String	strName = mData.get(position);
+			viewHolder.name_view.setText(strName);
+			
+			return convertView;
+		}
+		
 	}
 }
