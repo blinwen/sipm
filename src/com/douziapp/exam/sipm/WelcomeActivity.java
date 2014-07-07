99ae57be87cb5ada18b0a87e0ca0a79cc98c2d23
wenbaolin <blinwen@gmail.com>
2014-07-07 08:06:48 +0800

    modify

---
+++ src/com/douziapp/exam/sipm/WelcomeActivity.java
@@ -0,0 +1,37 @@
+package com.douziapp.exam.sipm;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class WelcomeActivity extends Activity {
+
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.activity_welcome);
+		
+		new Handler().postDelayed(new StartMain(), 	1500);
+	}
+
+	
+	private class StartMain extends Thread{
+
+		@Override
+		public void run() {
+
+			super.run();
+			
+			Intent intent = new Intent(WelcomeActivity.this,MainActivity.class);
+			
+			//startActivity(intent);
+			
+			//WelcomeActivity.this.finish();
+		}
+		
+	}
+}
