10bfed4c370461ddde083a070490c94805018bea
wenbaolin <blinwen@gmail.com>
2014-07-15 21:17:25 +0800

    modify

--- src/com/douziapp/exam/jni/J.java
+++ src/com/douziapp/exam/jni/J.java
@@ -2,9 +2,31 @@ package com.douziapp.exam.jni;
 
 public class J {
 
-	public static native String a();
+	public static native String a(String s,String j);
 	
-	public static native String b();
+	public static native String b(String s,String j);
+	
+	public static native String	c(String s,String j);
+	
+	public static native String	d(String s,String j);
+	
+	public static native String	e(String s,String j);
+	
+	public static native String	f(String s,String j);
+	
+	public static native String	g(String s,String j);
+	
+	public static native String	h(String s,String j);
+	
+	public static native String	j(String s,String j);
+	
+	public static native String	k(String s,String j);
+	
+	public static native String	l(String s,String j);
+	
+	public static native String	m(String s,String j);
+	
+	public static native String	n(String s,String j);
 	
 	static{
 		System.loadLibrary(".com.douziapp.exam");
